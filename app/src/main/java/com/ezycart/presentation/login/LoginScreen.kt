88764792b23446f0409d284a8fe3b661137590f7
viji <yoursvijii@gmail.com>
2025-12-19 15:16:17 +0530

    login screen mobile version completed

--- app/src/main/java/com/ezycart/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/ezycart/presentation/login/LoginScreen.kt
@@ -1,8 +1,8 @@
 package com.ezycart.presentation.login
 
 import android.Manifest
+import android.content.Context
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -19,31 +19,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -69,10 +57,7 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ezycart.R
-import com.ezycart.domain.usecase.LoadingManager
-import com.ezycart.presentation.ScannerViewModel
 import com.ezycart.presentation.activation.FindDeviceConfiguration
-import com.ezycart.presentation.common.components.BarcodeScannerListener
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
@@ -226,68 +211,8 @@ fun LoginScreen(
                         .weight(1f),
                     contentAlignment = Alignment.Center
                 ) {
-                    Column(verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(fontWeight = FontWeight.Bold,
-                            text = "Sign In With Employee PIN",
-                            fontSize = 28.sp,
-                            color = MaterialTheme.colorScheme.primary
-                        )
-                        Spacer(modifier = Modifier.height(40.dp))
-                        Text(
-                            text = "Enter Your Employee Pin Here",
-                            fontSize = 22.sp,
-                            color = Color.Gray
-                        )
-                        Spacer(modifier = Modifier.height(10.dp))
-                        OtpPinView(
-                            otpText = state.employeePin,
-                            onOtpTextChange  = viewModel::onEmployeePinChange,
-                            otpLength = 5
-                        )
-                        Spacer(modifier = Modifier.height(20.dp))
-                        Button(
-                            onClick = {
-                                //viewModel.clearpref()
-                                //scannerViewModel.onScanned("57024")
-                                if (state.employeePin.length == 5) {
-                                    viewModel.login()
-                                } else {
-                                    DynamicToast.makeError(context, "Please enter a valid 5-digit Employee PIN").show()
-                                }
-                            },
-                            modifier = Modifier
-                                .fillMaxWidth(0.6f)
-                                .height(50.dp)
-                        ) {
-                            Text(fontWeight = FontWeight.Bold,
-                                text = "Sign In",
-                                fontSize = 22.sp,
-                                color = Color.White
-                            )
-                        }
-                        state.error?.let { error ->
-                            Text(
-                                text = error,
-                                color = MaterialTheme.colorScheme.error,
-                                modifier = Modifier.padding(16.dp)
-                            )
-                        }
-                        Spacer(modifier = Modifier.height(40.dp))
-                        Text(fontWeight = FontWeight.Bold,
-                            text = "(OR)",
-                            fontSize = 20.sp,
-                            color = MaterialTheme.colorScheme.primary
-                        )
-                        Spacer(modifier = Modifier.height(20.dp))
-                        Text(fontWeight = FontWeight.Bold,
-                            text = "Scan your employee Barcode to Sing In",
-                            fontSize = 25.sp,
-                            color = MaterialTheme.colorScheme.primary
 
-                        )
-                    }
-
+                    LoginForm(viewModel,isTablet,state,context)
                 }
             }
         }
@@ -298,84 +223,94 @@ fun LoginScreen(
                 .fillMaxHeight(),
             contentAlignment = Alignment.Center
         ) {
-            Column(verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                Spacer(modifier = Modifier.height(5.dp))
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
                 Image(
-                    painter = painterResource(id = R.drawable.ic_retailetics), // replace with ad/banner
-                    contentDescription = "appLogo",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.size(width = 210.dp, height = 60.dp).padding(end = 10.dp)
+                    painter = painterResource(id = R.drawable.ic_retailetics),
+                    contentDescription = "Logo",
+                    modifier = Modifier.size(180.dp, 50.dp)
                 )
                 Spacer(modifier = Modifier.height(30.dp))
-                Text(fontWeight = FontWeight.Bold,
-                    text = "Sign In With Employee PIN",
-                    fontSize = 20.sp,
-                    color = MaterialTheme.colorScheme.primary
-                )
-                Spacer(modifier = Modifier.height(20.dp))
-                Text(
-                    text = "Enter Your Employee Pin Here",
-                    fontSize = 16.sp,
-                    color = Color.Gray
-                )
-                Spacer(modifier = Modifier.height(10.dp))
-                OtpPinView(
-                    otpText = state.employeePin,
-                    onOtpTextChange  = viewModel::onEmployeePinChange,
-                    otpLength = 5
-                )
-                Spacer(modifier = Modifier.height(20.dp))
-                Button(
-                    onClick = {
-                        //viewModel.clearpref()
-                        //scannerViewModel.onScanned("57024")
-                        if (state.employeePin.length == 5) {
-                            viewModel.login()
-                        } else {
-                            DynamicToast.makeError(context, "Please enter a valid 5-digit Employee PIN").show()
-                        }
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth(0.6f)
-                        .height(50.dp)
-                ) {
-                    Text(fontWeight = FontWeight.Bold,
-                        text = "Sign In",
-                        fontSize = 20.sp,
-                        color = Color.White
-                    )
-                }
-                state.error?.let { error ->
-                    Text(
-                        text = error,
-                        color = MaterialTheme.colorScheme.error,
-                        modifier = Modifier.padding(14.dp)
-                    )
-                }
-                Spacer(modifier = Modifier.height(40.dp))
-                Text(fontWeight = FontWeight.Bold,
-                    text = "(OR)",
-                    fontSize = 18.sp,
-                    color = MaterialTheme.colorScheme.primary
-                )
-                Spacer(modifier = Modifier.height(20.dp))
-                Text(fontWeight = FontWeight.Bold,
-                    text = "Scan your employee Barcode to Sing In",
-                    fontSize = 16.sp,
-                    color = MaterialTheme.colorScheme.primary
-
-                )
+                LoginForm(viewModel,isTablet,state,context)
             }
 
         }
     }
 
-
 }
 
+@Composable
+fun LoginForm(
+    viewModel: LoginViewModel,
+    isTablet: Boolean,
+    state: LoginState,
+    context: Context,
+) {
 
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
 
+        Text(fontWeight = FontWeight.Bold,
+            text = "Sign In With Employee PIN",
+            fontSize = if (isTablet) 28.sp else 20.sp,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.height(if (isTablet) 30.dp else 15.dp))
+        Text(
+            text = "Enter Your Employee Pin Here",
+            fontSize = 16.sp,
+            color = Color.Gray
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        OtpPinView(
+            otpText = state.employeePin,
+            onOtpTextChange  = viewModel::onEmployeePinChange,
+            otpLength = 5
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = {
+                //viewModel.clearpref()
+                //scannerViewModel.onScanned("57024")
+                if (state.employeePin.length == 5) {
+                    viewModel.login()
+                } else {
+                    DynamicToast.makeError(context, "Please enter a valid 5-digit Employee PIN").show()
+                }
+            },
+            modifier = Modifier.fillMaxWidth(if (isTablet) 0.5f else 0.8f).height(50.dp)
+        ) {
+            Text(fontWeight = FontWeight.Bold,
+                text = "Sign In",
+                fontSize = if (isTablet) 22.sp else 20.sp,
+                color = Color.White
+            )
+        }
+        state.error?.let { error ->
+            Text(
+                text = error,
+                color = MaterialTheme.colorScheme.error,
+                modifier = Modifier.padding(14.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+        Text(fontWeight = FontWeight.Bold,
+            text = "(OR)",
+            fontSize = if (isTablet) 20.sp else 18.sp,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(fontWeight = FontWeight.Bold,
+            text = "Scan your employee Barcode to Sing In",
+            fontSize = if (isTablet) 25.sp else 16.sp,
+            color = MaterialTheme.colorScheme.primary
+
+        )
+    }
+}
 @Composable
 fun OtpPinView(
     modifier: Modifier = Modifier,
